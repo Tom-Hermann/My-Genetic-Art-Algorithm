9aaa2d6eaa30abb2f1c9f3e0c14865dbeca80e9c
Tom Hermann <tom@predictalab.com>
2022-12-12 16:18:14 +0000

    new image + multithreading

--- Population.py
+++ Population.py
@@ -2,7 +2,9 @@ from painting import Painting
 import random
 from PIL import Image, ImageDraw
 from copy import deepcopy
+import threading
 
+SAVE = 0
 
 def pick_random(pop):
     mom = random.choice(pop)
@@ -19,18 +21,23 @@ def pick_best_and_random(pop: 'Population', maximize=False):
     return mom, dad
 
 
-def print_summary(pop: 'Population', img_template="output%d.png", checkpoint_path="output") -> Painting:
+def print_summary(pop: 'Population', img_template="output%d.png", checkpoint_path="output") -> int:
     avg_fitness = sum(i.fiting for i in pop.induviduals) / pop._population_size
 
     print(f"generation {pop.current_generation}, best score {pop._current_best.fiting}, pop. avg. {avg_fitness}")
-    img = pop._current_best.draw()
-    img.save(img_template % pop.current_generation, 'PNG')
+    if (SAVE != pop._current_best.fiting):
+        SAVE = pop._current_best.fiting
+        img = pop._current_best.draw()
+        img.save(img_template % pop._id_image, 'PNG')
+        pop._id_image += 1
+
 
     # if pop.current_generation % 50 == 0:
     #     pop.checkpoint(target=checkpoint_path, id=pop.current_generation)
 
 class Population():
     def __init__(self, target_img, population_size= 50, generation=0, nb_figures=100, background_color=None, nb_corner=-1, rate=0.04, swap=0.5, sigma=1, color_rate=0.1) -> None:
+        self._id_image = 0
         self._population_size: int = population_size
         self.induviduals: list[Painting] = [Painting(nb_figures, target_img, background_color, nb_corner) for _ in range(population_size)]
         self.current_generation: int = generation
@@ -133,7 +140,14 @@ class Population():
             self.color_rate=color_rate
         for _ in range(nb_generation):
             self.evolve()
-            print_summary(self, img_template, checkpoint_path)
+            #  mutltithreading print_summary(self, img_template, checkpoint_path) using threading lib:
+            threading.Thread(target=print_summary, args=(self, img_template, checkpoint_path)).start()
+
+
+
+
+
+
 
     def checkpoint(self, target, id):
         with open(target + str(id) + '.txt', 'w') as f:
--- evolution.py
+++ evolution.py
@@ -1,18 +1,24 @@
 from Population import Population
 import os
 from PIL import Image
+import sys
 
-target_image_path = "./img/moi2.jpg"
-checkpoint_path = "./res/"
+target_image_path = sys.argv[1]
+checkpoint_path = sys.argv[2]
+nb_corner = int(sys.argv[3])
+# target_image_path = "./img/moi2.jpg"
+# checkpoint_path = "./res/"
 image_template = os.path.join(checkpoint_path, "drawing_%05d.png")
 target_image = Image.open(target_image_path).convert('RGBA')
 
 num_triangles = 150
 population_size = 100
 
-pop = Population(target_image, population_size=population_size, nb_figures=num_triangles, nb_corner=3, rate=0.20, swap=0.75)
+print("Init population:")
+pop = Population(target_image, population_size=population_size, nb_figures=num_triangles, nb_corner=nb_corner, rate=0.20, swap=0.75)
+print("Finish init")
 pop.evolution(300, img_template=image_template, checkpoint_path=checkpoint_path, rate=0.20, swap=0.75, color_rate=0.15)
-pop.evolution(500, img_template=image_template, checkpoint_path=checkpoint_path, rate=0.15, swap=0.5, color_rate=0.1)
-pop.evolution(1000, img_template=image_template, checkpoint_path=checkpoint_path, rate=0.10, swap=0.25, color_rate=0.05)
-pop.evolution(1500, img_template=image_template, checkpoint_path=checkpoint_path, rate=0.05, swap=0, sigma=0.15, color_rate=0.01)
-pop.evolution(2000, img_template=image_template, checkpoint_path=checkpoint_path, rate=0.03, swap=0, sigma=0.12, color_rate=0.01)
+pop.evolution(500, img_template=image_template, checkpoint_path=checkpoint_path, rate=0.15, swap=0.5, color_rate=0)
+pop.evolution(1000, img_template=image_template, checkpoint_path=checkpoint_path, rate=0.10, swap=0.25, color_rate=0)
+pop.evolution(1500, img_template=image_template, checkpoint_path=checkpoint_path, rate=0.05, swap=0, sigma=0.15, color_rate=0)
+pop.evolution(2000, img_template=image_template, checkpoint_path=checkpoint_path, rate=0.03, swap=0, sigma=0.12, color_rate=0)
